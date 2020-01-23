0eb5d0c1548efc0410a06fe941017242fb010a80
Vamp <vampiirecodes@gmail.com>
2020-01-23 14:18:12 -0600

    global: add boilerplate

---
+++ src/main/java/org/launchcode/devops/certauthority/CertAuthorityApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.devops.certauthority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertAuthorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertAuthorityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/devops/certauthority/CertAuthorityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.devops.certauthority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertAuthorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
